1d4a73b5a669f8bdb81ae166a0618284e9333d77
Sakshi <sakshiraj1705@gmail.com>
2022-04-13 20:27:15 +0530

    Assignment-4

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './Postview.css';
+import React, { useState, useEffect } from "react";
+import Post from "./Post/Post";
+// import "./Postview.css";
 
+const Postview = () => {
+  const [posts, setPosts] = useState([]);
 
-const Postview=()=> {
+  const setPostsAsync = async () => {
+    const response = await fetch("http://localhost:3004/user"),
+      data = await response.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    setPostsAsync();
+  }, []);
+
   return (
-    <div className="site-container">
+    <div className="Postview">
+      {posts.map((post, index) => (
+        <Post key={index} config={post} />
+      ))}
     </div>
   );
-}
+};
 
 export default Postview;
